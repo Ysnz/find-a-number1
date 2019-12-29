48e64a8b9195302000a701296a0a55e3502df286
Ysnz <55712458+Ysnz@users.noreply.github.com>
2019-12-29 14:43:30 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
 let nr_game;
 let counter = 0;
-/* GET RANDOM NUMBER */
+
 let number = Math.floor((Math.random() * 100) + 1);
 
 document.getElementById("check_it").addEventListener("click", function(){
     
   check_number();
   });
- /* ENTER FOR CHECKING NUMBER */ 
+
 document.getElementById("nr_game").addEventListener("keyup", function(event){
   if(event.which === 13){
     check_number();
     
   }
 });
-/* CHECK NUMBER */
+
   function check_number(){
     counter++;
   document.getElementById("count_it").innerHTML= "This is your test number:" +counter;
@@ -23,8 +23,8 @@ document.getElementById("nr_game").addEventListener("keyup", function(event){
   if(nr_game == number){
  
     alert("You are the winner :D. "+"You guest after"+ (counter--) +" times!");
-   /* NEXT GAME */
-    var next_game = confirm("Chcesz zagrać jeszcze raz?");
+   
+    var next_game = confirm("You wanna play again?");
       if (next_game == true){
      number = Math.floor((Math.random() * 100) + 1);
       counter=0;    document.getElementById("answer").innerHTML="";
