4c4e0506e02d26b0c9bbd0de30e738b0ba050de6
Ysnz <55712458+Ysnz@users.noreply.github.com>
2019-12-29 14:29:51 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,61 @@
+let nr_game;
+let counter = 0;
+/* GET RANDOM NUMBER */
+let number = Math.floor((Math.random() * 100) + 1);
+
+document.getElementById("check_it").addEventListener("click", function(){
+    
+  check_number();
+  });
+ /* ENTER FOR CHECKING NUMBER */ 
+document.getElementById("nr_game").addEventListener("keyup", function(event){
+  if(event.which === 13){
+    check_number();
+    
+  }
+});
+/* CHECK NUMBER */
+  function check_number(){
+    counter++;
+  document.getElementById("count_it").innerHTML= "This is your test number:" +counter;
+ 
+    nr_game = document.getElementById("nr_game").value;
+  if(nr_game == number){
+ 
+    alert("You are the winner :D. "+"You guest after"+ (counter--) +" times!");
+   /* NEXT GAME */
+    var next_game = confirm("Chcesz zagrać jeszcze raz?");
+      if (next_game == true){
+     number = Math.floor((Math.random() * 100) + 1);
+      counter=0;    document.getElementById("answer").innerHTML="";
+         document.getElementById("count_it").innerHTML="";
+      }
+    else {
+ document.getElementById("count_it").innerHTML=""; 
+      document.getElementById("answer").innerHTML="Thanks for game!";
+     
+    }
+  }
+    /* CHECK IF VALUE IS A NUMBER */
+  else if(nr_game==""){
+     document.getElementById("answer").innerHTML="Weite the number";
+  }
+  else if(nr_game.indexOf(',') != -1 || nr_game.indexOf('.') != -1){
+     document.getElementById("answer").innerHTML="This is NOT a natural number";
+  }
+ 
+  else if(nr_game > number){
+    document.getElementById("answer").innerHTML="I am thinking about lower number";
+  }
+  else if(nr_game < number) { 
+    document.getElementById("answer").innerHTML="I am thinking about higher number";
+  }
+  else if(isNaN(nr_game)){
+    document.getElementById("answer").innerHTML="Are you sure that this is a number?";
+  }
+ 
+document.getElementById("nr_game").value='';
+document.getElementById("nr_game").focus();
+  }
+
+
